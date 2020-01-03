468f02a7b6a806256514f54681e625608fc68243
Yao Jian Yap <yapyaojian731@gmail.com>
2020-01-03 16:04:16 +0800

    Implemented custom Toolbar n Changed styles

--- app/src/main/java/com/yawjenn/mvvmpractice/MainActivity.kt
+++ app/src/main/java/com/yawjenn/mvvmpractice/MainActivity.kt
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.commit
 import com.yawjenn.mvvmpractice.home.HomeFragment
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        setSupportActionBar(tToolbar)
 
         supportFragmentManager.commit {
             add(R.id.container, HomeFragment.newInstance())
